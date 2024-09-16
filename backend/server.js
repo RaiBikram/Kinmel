b061f7b0ec60c2e057a29dfdcfa35d60a1a4e7a5
Bikram Rai <bikramdevlopment@gmail.com>
2024-09-16 12:46:52 +0545

    add root to test

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the API!');
+});
+
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
