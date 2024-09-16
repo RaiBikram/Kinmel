5ed9f5022319c3e997ea20e196327b7ae90b6933
Bikram Rai <bikramdevlopment@gmail.com>
2024-09-16 14:16:22 +0545

    fixed audit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:process.env.CORS_ORIGIN,
+    origin:"*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
